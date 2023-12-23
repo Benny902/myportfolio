470ee21027ec5a7b6bacae210aa9382a4d8fa70d
Benny <Benny902@gmail.com>
2023-12-23 05:02:03 +0200

    added more projects and some adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,37 @@
-function toggleDiv(divid)
-  {
- 
-    var1 = divid + '1';
-    var2 = divid + '2';
-    var3 = divid + '3';
-    var4 = divid + '4';
- 
-    if(document.getElementById(var1).style.display == 'block') {
-        document.getElementById(var1).style.display = 'none';
-        document.getElementById(var2).style.display = 'block';
-        document.getElementById(var3).style.display = 'none';
-        document.getElementById(var4).style.display = 'none';
-      }
-   
-    else if(document.getElementById(var2).style.display == 'block') {
-        document.getElementById(var1).style.display = 'none';
-        document.getElementById(var2).style.display = 'none';
-        document.getElementById(var3).style.display = 'block';
-        document.getElementById(var4).style.display = 'none';
-      }
-    else if(document.getElementById(var3).style.display == 'block') {
-      document.getElementById(var1).style.display = 'none';
-      document.getElementById(var2).style.display = 'none';
-      document.getElementById(var3).style.display = 'none';
-      document.getElementById(var4).style.display = 'block';
-    }
-    
-    else
-    {  
-      document.getElementById(var1).style.display = 'block';
-      document.getElementById(var2).style.display = 'none';
-      document.getElementById(var3).style.display = 'none';
-      document.getElementById(var4).style.display = 'none';
-    }
-} 
 
-function toggleDiv2(divid)
-  {
- 
-    varon = divid + 'on';
-    varoff = divid + 'off';
- 
-    if(document.getElementById(varon).style.display == 'block')
-    {
-    document.getElementById(varon).style.display = 'none';
-    document.getElementById(varoff).style.display = 'block';
-    }
-   
-    else
-    {  
-    document.getElementById(varoff).style.display = 'none';
-    document.getElementById(varon).style.display = 'block'
-    }
-} 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("myProjects");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+var slideIndex2 = 1;
+showDivs2(slideIndex2);
+
+function plusDivs2(n) {
+  showDivs2(slideIndex2 += n);
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("myProjects2");
+  if (n > x.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex2-1].style.display = "block";  
+}
